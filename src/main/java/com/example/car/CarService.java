5d1b089bbb034e9de8c15a438d3fe4d6c983a2e6
mjlslink <mjlslink5@gmail.com>
2024-10-19 15:53:38 -0600

    added unique constraint

--- src/main/java/com/example/car/CarService.java
+++ src/main/java/com/example/car/CarService.java
@@ -2,6 +2,8 @@ package com.example.car;
 
 import com.example.car.model.Car;
 import com.example.car.model.Mapper;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +13,7 @@ import java.util.stream.Collectors;
 @Service
 public class CarService {
 
+    private final Logger logger = LogManager.getLogger();
     private final CarRepositorry carRepositorry;
 
     @Autowired
@@ -19,6 +22,7 @@ public class CarService {
     }
 
     public List<Car> getAllCars() {
+        logger.info("Getting all cars");
         return carRepositorry.findAll().stream()
                 .map(Mapper::toDto)
                 .collect(Collectors.toList());
--- src/main/resources/cars.ddl
+++ src/main/resources/cars.ddl
@@ -4,4 +4,6 @@ create table cars (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   model_name VARCHAR(200) NOT NULL,
   manufacturer VARCHAR(200),
-  vin VARCHAR(200) NOT NULL);
+  vin VARCHAR(200) NOT NULL,
+  constraint uq1 unique (model_name)
+);
